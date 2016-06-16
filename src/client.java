8fa8701bd7b38e7c7f4265279bcdfc28b9fb43a8
Dion <dfengels@gmail.com>
2016-06-16 14:28:06 +0200

    Changes to client

--- src/client.java
+++ src/client.java
@@ -1,6 +1,7 @@
-import clustering.Clusterer;
+import clustering.Clustering;
 import loading.DataLoader;
 import models.Cluster;
+import models.Solution;
 
 import java.io.IOException;
 
@@ -9,16 +10,14 @@ public class client {
         DataLoader loader = new DataLoader();
         loader.loadData();
 
-        Clusterer cluster = new Clusterer(3, loader.getDataSet());
-        cluster.execute();
+        Clustering cluster = new Clustering(3, loader.getDataSet());
+        Solution solution = cluster.execute(50);
 
-        for(Cluster c : cluster.getClusters()){
+        System.out.println("Total SSE: "+solution.getSSE());
+        for(Cluster c : solution.getClusters()){
+            System.out.println("-------------------------------------");
         	System.out.println("The Sum of Squared Errors in this cluster: "+c.getSSE()+"\n");
             System.out.println(c.getPrintData());
-            System.out.println("-------------------------------------");
-        	//System.out.println("SSE: "+c.getSSE()+", size: "+c.getMembers().size()+", "+"\n"+c.getCentroid());
         }
-        
-        
     }
 }
